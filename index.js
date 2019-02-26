6990fbf1db968577c4ef097751aba923b66a3dac
Jorge Ordoñez <jorgeorm@gmail.com>
2019-02-25 19:56:44 -0500

    Styles: Adding postcss commonly used plugins

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const styles = require('./src/styles');
 const javascript = require('./src/javascript');
 const utils = require('./src/utils');
+const postcss = require('./src/postcss');
 
 module.exports = {
     styles,
     javascript,
     utils,
+    postcss,
 };
---
+++ src/postcss.js
@@ -0,0 +1,8 @@
+module.exports = exports = {};
+
+/**
+ * Commonly used postcss plugins when dealing with CSS
+ */
+exports.PLUGINS = [
+    require('autoprefixer'),
+]

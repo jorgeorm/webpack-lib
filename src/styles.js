03a0657ef4ac33b5390ea77681c08b42be3fe808
Jorge Ordoñez <jorgeorm@gmail.com>
2019-02-25 19:07:43 -0500

    Styles: Adding SASS loaders setup and refactor for additional rules

--- src/styles.js
+++ src/styles.js
@@ -8,27 +8,42 @@ const { ENV_DEV, ENV_PROD, ENV_STG } = require('./constants');
  * Setup postcss for the features you want. @see {@link https://github.com/postcss/postcss-loader}
  * 
  * @param {string} [env=ENV_DEV]
+ * @param {number} importLoaders importLoaders cahnges the behavior of the loaders by allowing to import .scss or .less or whatever other loader in a @import
  * @returns {array}
  */
-function cssLoaders(env = ENV_DEV) {
+function cssLoaders(env = ENV_DEV, importLoaders = 1) {
     return [
         {
             loader: 'css-loader', // translates CSS into CommonJS
             options: {
                 sourceMap: env === ENV_PROD || env === ENV_STG, // Loads sourcemaps if exists
-                importLoaders: 1,
+                importLoaders,
             }
-            // Note: importLoaders cahnges the behavior of the loaders by allowing to import
-            // .scss or .less or whatever other loader in an import
-            // importLoaders: 2, // 0 => no loaders (default);
-            // 1 => postcss-loader;
-            // 2 => postcss-loader, another-loader
         },
         {
             loader: 'postcss-loader', // Postprocessor, transforms css
             options: { sourceMap: env === ENV_PROD || env === ENV_STG } // Loads sourcemaps if exists
         }
     ];
+}
+
+/**
+ * Contains common configuration for loading sass code
+ * @param {string} [env] - Represents the environment that's going to parse the config 'production', 'staging', 'development'
+ * @param {array} baseLoaders - Array of loaders to be used after loading the specific sass files
+ */
+function sassLoaders(env = DEV, baseLoaders = cssLoaders(env, 0)) {
+    return ([
+        ...baseLoaders,
+        {
+            loader: 'sass-loader', // compiles Sass to CSS
+            options: { sourceMap: env === ENV_PROD || env === ENV_STG } // Loads sourcemaps if exists
+            // In case is required to pass env vars.
+            // options: {
+            //     data: '$env: ' + process.env.NODE_ENV + ';'
+            // }
+        }
+    ]);
 }
 
 /**
@@ -40,7 +55,7 @@ function cssLoaders(env = ENV_DEV) {
  * @param {string | array} [css.exclude] - Directories that are not going to be parsed by the css loader
  * @return {object} - Webpack configuration for the vanilla css loader
  */
-exports.loadCSS = ({ include, exclude, loaders = cssLoaders() } = {}) => {
+exports.loadCSS = ({ loaders = cssLoaders(), include, exclude } = {}) => {
     if (typeof loaders === 'undefined' || !loaders) throw Error('An array of loaders must be provided');
     
     const rootLoaders = [ 'style-loader' ];
@@ -51,8 +66,42 @@ exports.loadCSS = ({ include, exclude, loaders = cssLoaders() } = {}) => {
                 test: /\.css$/,
                 include,
                 exclude,
-                use: rootLoaders.concat(loaders)
+                rules: [
+                    // Garantees style-loader is called only if asset is called by js
+                    {
+                        issuer: /\.jsx?$/,
+                        loader: 'style-loader',
+                    },
+                    { use: loaders }
+                ]
             }]
         }
     };
 }
+
+/**
+ * Configures webpack loader for app wide vanilla css, uses style-loader since implements HMR
+ * 
+ * @param {object} sass - sass loader configuration object
+ * @param {array} sass.loaders - Array of loaders that will be used to parse the rule, by default will be the sassLoaders function
+ * @param {string | array} [sass.include] - Directory to be scanned for vanilla sass code
+ * @param {string | array} [sass.exclude] - Directories that are not going to be parsed by the sass loader
+ * @return {object} - Webpack configuration for the vanilla sass loader
+ */
+exports.loadSASS = ( { loaders = sassLoaders(), include, exclude } ) => ({
+    module: {
+        rules: [{
+            test: /\.s(c|a)ss$/,
+            include,
+            exclude,
+            rules: [
+                // Garantees style-loader is called only if asset is called by js
+                {
+                    issuer: /\.jsx?$/,
+                    loader: 'style-loader',
+                },
+                { use: loaders }
+            ]
+        }]
+    }
+})

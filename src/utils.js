09bf507c9b9206d3bf1182b4c2184099e2392c0b
Jairo Valderrama <jairovg@gmail.com>
2020-04-04 17:39:07 -0500

    Allows to change stats level in dev server

--- src/utils.js
+++ src/utils.js
@@ -10,31 +10,32 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
  * @param {object} server - The server that's going to be started
  * @param {string} server.host - Host were is going to be run ie: 0.0.0.0
  * @param {number} server.port - Port number were the server is going to listen ie: 8080
+ * @param {stats} server.stats - Bundle information that gets displayed
  * @param {boolean} server.historyApiFallback - Enables history API when using html5HistoryApi routing.
  * @param {boolean} server.hmr - Enables hot module replacement in server
  * @return {object} - Webpack configuration for the Dev server
  */
-exports.webpackDevServer = ({ host, port, historyApiFallback = false, hmr = false } = {}) => {
+exports.webpackDevServer = ({ host, port, stats = 'errors-only', historyApiFallback = false, hmr = false } = {}) => {
     const wdsConfig = {
         devServer: {
             // When using docker, vagrant or virtualization set
             // host || '0.0.0.0' it will be available to all network devices
             host,
             port,
-    
+
             // Displays only errors to reduce the amount of output
-            // values: errors-only | minimal
-            stats: 'errors-only',
-    
+            // values: 'none' | 'errors-only' | 'minimal' | 'normal' | 'verbose'
+            stats,
+
             // Opens the browser once dev-server is up and running
             open: true,
-    
+
             // Shows a full-screen overlay in the browser when there are compiler errors or warnings
             overlay: {
                 errors: true,
                 warnings: true,
             },
-    
+
             // For when you are using HTML5 History API based routing.
             historyApiFallback,
         },
@@ -50,7 +51,7 @@ exports.webpackDevServer = ({ host, port, historyApiFallback = false, hmr = fals
 
 /**
  * Adds plugin for generating html by using html-webpack-plugin
- * 
+ *
  * @param {object} config - @see https://github.com/jantimon/html-webpack-plugin
  */
 exports.htmlPlugin = (config = {}) => ({
